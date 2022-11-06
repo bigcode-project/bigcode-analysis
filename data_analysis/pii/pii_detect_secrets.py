08d98acb919e7dd6dd414a4c0b3caf67fd3ecedc
Jia LI <lijia0765@gmail.com>
2022-11-06 21:45:21 +0800

    clean up

--- data_analysis/pii/pii_detect_secrets.py
+++ data_analysis/pii/pii_detect_secrets.py
@@ -4,18 +4,19 @@ from detect_secrets import SecretsCollection
 from detect_secrets.settings import default_settings
 
 
-def scan_str_content(content, suffix):
-    fp = tempfile.NamedTemporaryFile(suffix=suffix, delete=False, mode='w')
-    fp.write(content)
+def scan_str_for_secrets(text):
+    '''Write the text to a temporary file and scan it for secrets.'''
+    fp = tempfile.NamedTemporaryFile(delete=False, mode='w')
+    fp.write(text)
     fp.close()
     secrets = SecretsCollection()
     with default_settings():
         secrets.scan_file(fp.name)
     os.unlink(fp.name)
-    secrets_set = list(secrets.data.values())
+    secrets_dict = list(secrets.data.values())
     result = []
-    if secrets_set:
-        for secret in secrets_set[0]:
+    if secrets_dict:
+        for secret in secrets_dict[0]:
             result.append({
                 'type': secret.type,
                 'line_number': secret.line_number,
