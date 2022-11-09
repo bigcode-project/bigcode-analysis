c6e6882a85c4ce377947657c80b30172178074a1
Jia LI <lijia0765@gmail.com>
2022-11-09 12:38:20 +0800

    add details forevaluation

--- data_analysis/pii/evaluate.py
+++ data_analysis/pii/evaluate.py
@@ -1,4 +1,5 @@
 import json
+import pandas as pd
 
 
 def overlapped(a, b):
@@ -40,21 +41,63 @@ def compare_intervals(ref_intervals, pred_intervals):
     return metrics, ref_matched, pred_matched
 
 
-def evaluate_pii_metrics(dataset, pred_column='secrets', ref_column="pii"):
+def evaluate_pii_metrics(dataset, pred_column='secrets', ref_column="pii", return_details=False):
     """Evaluate the metrics for the PII detection task
+    Returns
+    -------
+    metric_df : pd.DataFrame
+        A dataframe with the metrics
+
+    detail_metrics : dict of pd.DataFrame
+        A dictionary with the metrics for each tag per sample
+
+    details : dict
+        A dictionary containing the details of the evaluation
+        ex. details[sample_id][tag]['TP_ref'] contains the list of true positives for the reference
     """
     tags = ['EMAIL', 'IP_ADDRESS', 'SSH_KEY', 'API_KEY', 'NAME', 'USERNAME', 'PASSWORD', 'AMBIGUOUS']
     metrics_dict = {}
+    if return_details:
+        details = {}
     for tag in tags:
         metrics_dict[tag] = {'TP_ref': 0, 'TP_pred': 0, 'FN': 0, 'FP': 0}
     for i in range(len(dataset)):
         row = dataset[i]
         ref_list = json.loads(row[ref_column])
         pred_list = json.loads(row[pred_column])
+        if return_details:
+            details[i] = {}
         for tag in tags:
-            ref_intervals = [(e['start'], e['end']) for e in ref_list if e['tag'] == tag]
-            pred_intervals = [(e['start'], e['end']) for e in pred_list if e['tag'] == tag]
-            metrics_i, _, _ = compare_intervals(ref_intervals, pred_intervals)
+            ref_list_tag = [e for e in ref_list if e['tag'] == tag]
+            pred_list_tag = [e for e in pred_list if e['tag'] == tag]
+
+            ref_intervals = [(e['start'], e['end']) for e in ref_list_tag]
+            pred_intervals = [(e['start'], e['end']) for e in pred_list_tag]
+
+            metrics_i, ref_matched, pred_matched = compare_intervals(ref_intervals, pred_intervals)
+
             for k in metrics_dict[tag]:
                 metrics_dict[tag][k] += metrics_i[k]
-    return metrics_dict
+            if return_details:
+                # i am expert python programmer
+                details_i = {
+                    'TP_ref': [ref_list_tag[j] for j in range(len(ref_list_tag)) if ref_matched[j]],
+                    'TP_pred': [pred_list_tag[j] for j in range(len(pred_list_tag)) if pred_matched[j]],
+                    'FN': [ref_list_tag[j] for j in range(len(ref_list_tag)) if not ref_matched[j]],
+                    'FP': [pred_list_tag[j] for j in range(len(pred_list_tag)) if not pred_matched[j]],
+                }
+                details[i][tag] = details_i
+    metric_df = pd.DataFrame(metrics_dict).T
+    if return_details:
+        detail_metrics = {}
+        for tag in tags:
+            detail_metrics[tag] = []
+            for i in range(len(dataset)):
+                detail_metrics[tag].append({
+                    'TP_ref': len(details[i][tag]['TP_ref']),
+                    'TP_pred': len(details[i][tag]['TP_pred']),
+                    'FN': len(details[i][tag]['FN']),
+                    'FP': len(details[i][tag]['FP']),
+                })
+            detail_metrics[tag] = pd.DataFrame(detail_metrics[tag]).T
+    return metric_df if not return_details else (metric_df, detail_metrics, details)
