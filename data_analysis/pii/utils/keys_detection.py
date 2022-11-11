b113bfad10f165942814b042ccf90c320435c1a1
loubnabnl <loubnabenallal1999@gmail.com>
2022-11-11 11:22:46 +0100

    add filters

--- data_analysis/pii/utils/keys_detection.py
+++ data_analysis/pii/utils/keys_detection.py
@@ -3,6 +3,7 @@ import tempfile
 
 from detect_secrets import SecretsCollection
 from detect_secrets.settings import transient_settings
+from gibberish_detector import detector
 
 # Secrets detection with detect-secrets tool
 
@@ -10,7 +11,6 @@ from detect_secrets.settings import transient_settings
 filters = [
     # some filters from [original list](https://github.com/Yelp/detect-secrets/blob/master/docs/filters.md#built-in-filters)
     # were removed based on their targets
-    {"path": "detect_secrets.filters.heuristic.is_sequential_string"},
     {"path": "detect_secrets.filters.heuristic.is_potential_uuid"},
     {"path": "detect_secrets.filters.heuristic.is_likely_id_string"},
     {"path": "detect_secrets.filters.heuristic.is_templated_secret"},
@@ -43,6 +43,56 @@ plugins = [
 ]
 
 
+def is_gibberish(matched_str):
+    """Checks to make sure the PII span is gibberish and not word like"""
+    # pip install gibberish-detector
+    # download the training corpora from https://raw.githubusercontent.com/domanchi/gibberish-detector/master/examples/big.txt
+    # run gibberish-detector train big.txt > big.model to generate the model (it takes 3 seconds)
+    Detector = detector.create_from_model('gibberish_data/big.model')
+    return Detector.is_gibberish(matched_str.lower())
+
+
+def is_hash(content, value):
+    """Second check if the value is a hash (after gibberish detector)"""
+    # get the line where value occured
+    lines = content[:content.index(value)].splitlines()
+    target_line = lines[-1]
+    if len(value) in [32, 40, 64]:
+        # if "sha" or "md5" are in content:
+        keywords = ["sha", "md5", "hash", "byte"]
+        if any(x in target_line.lower() for x in keywords):
+            return True
+
+def file_has_hashes(content, coeff = 0.02):
+    """Checks if the file contains literals 'hash' or 'sha' for more than 2% nb_of_lines"""
+    lines = content.splitlines()
+    count_sha = 0
+    count_hash = 0
+    nlines = content.count("\n")
+    threshold = int(coeff * nlines)
+    for line in lines:
+        count_sha += line.lower().count("sha")
+        count_hash += line.lower().count("hash")
+        if count_sha > threshold or count_hash > threshold:
+            return True
+    return False
+
+def get_indexes(text, value):
+    string = text
+    indexes = []
+    new_start = 0
+    while True:
+        try:
+            start = string.index(value)
+            indexes.append(new_start + start)
+            new_start = new_start + start + len(value)
+            string = text[new_start:]
+        except ValueError:
+            break
+    indexes = [(x, x + len(value)) for x in indexes]
+    return indexes
+
+
 def detect_keys(content, suffix=".txt"):
     """Detect secret keys in content using detect-secrets tool
     Args:
@@ -66,13 +116,27 @@ def detect_keys(content, suffix=".txt"):
     if secrets_set:
         for secret in secrets_set[0]:
             # TODO fix content.index: what if there are multiple occurences of the same key
-            matches.append(
-                {
-                    "tag": "KEY", # secret.type
-                    "value": secret.secret_value,
-                    "start": content.index(secret.secret_value),
-                    "end": content.index(secret.secret_value)
-                    + len(secret.secret_value),
-                }
-            )
+            if not is_gibberish(secret.secret_value):
+                print(f"False positive key (not gibberish): {secret.secret_value} with len {len(secret.secret_value)}")
+                continue
+            if is_hash(content, secret.secret_value):
+                print(f"False positive key (hash-gibberish): {secret.secret_value} with len {len(secret.secret_value)}")
+                continue
+            if file_has_hashes(content):
+                print(f"False positive key (file has hashes): {secret.secret_value} with len {len(secret.secret_value)}")
+                continue
+            indexes = get_indexes(content, secret.secret_value)
+            if len(indexes) > 1: print("Multiple occurences of the same key")
+            for start, end in indexes:
+                matches.append(
+                    {
+                        "tag": "KEY",
+                        "value": secret.secret_value,
+                        "start": start,
+                        "end": end,
+                    }
+                )
     return matches
+
+
+
