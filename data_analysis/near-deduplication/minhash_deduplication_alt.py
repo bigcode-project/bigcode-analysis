879f45be6ae21b8d60c980d55d107b1ab6a6120d
Chenghao Mou <mouchenghao@gmail.com>
2022-12-11 20:13:38 -0800

    minimize dependencies

--- data_analysis/near-deduplication/minhash_deduplication_alt.py
+++ data_analysis/near-deduplication/minhash_deduplication_alt.py
@@ -4,49 +4,170 @@
 # created     : 10/4/22
 from __future__ import annotations
 
+import hashlib
 import logging
 import os
-import random
 import re
+import struct
 import time
 import warnings
+from collections import defaultdict
+from itertools import tee
 from pathlib import Path
 from typing import Any
 from typing import Dict
+from typing import Iterable
 from typing import List
 from typing import Tuple
 
-warnings.filterwarnings("ignore", category=FutureWarning)
+with warnings.catch_warnings():
+    warnings.filterwarnings("ignore", category=FutureWarning)
+    import datasets
+    import numpy as np
+    import typer
+    from datasets import load_dataset
+    from scipy.integrate import quad as integrate
+    from tqdm import tqdm
 
-import datasets
-import typer
-from datasets import load_dataset
-from datasketch import MinHash
-from datasketch import MinHashLSH
-from nltk.util import ngrams
-from rich.console import Console
-from rich.logging import RichHandler
-from tqdm import tqdm
 
-random.seed(42)
-MINHASH_SEED = 42
+SEED = 42
 NON_ALPHA = re.compile("[^A-Za-z_0-9]")
-console = Console()
+RNG = np.random.RandomState(SEED)
+MAX_HASH = np.uint64((1 << 32) - 1)
+MERSENNE_PRIME = np.uint64((1 << 61) - 1)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-logger.addHandler(RichHandler(rich_tracebacks=True))
-logger.propagate = False
 datasets.logging.set_verbosity_error()
 
 
+def ngrams(sequence: List[str], n: int) -> Iterable:
+    """
+    Directly taken from nltk package to avoid dependency.
+
+    Parameters
+    ----------
+    sequence : list
+        The sequence of items to be n-grammed.
+    n : int
+        The order of the n-grams to be extracted.
+
+    Returns
+    -------
+    Iterable
+        The n-grams generated from the sequence.
+    """
+    iterables = tee(sequence, n)
+    for i, sub_iterable in enumerate(iterables):
+        for _ in range(i):
+            next(sub_iterable, None)
+    return zip(*iterables)
+
+
+def sha1_hash32(data):
+    """
+    Directly taken from datasketch package to avoid dependency.
+
+    Parameters
+    ----------
+    data : bytes
+
+    Returns
+    -------
+    int
+    """
+    return struct.unpack("<I", hashlib.sha1(data).digest()[:4])[0]
+
+
 def embed_func(
-    content: str, idx: int, *, num_perm: int, ngram_size: int, hashranges: List[Tuple[int, int]]
+    content: str,
+    idx: int,
+    *,
+    num_perm: int,
+    ngram_size: int,
+    hashranges: List[Tuple[int, int]],
+    permutations: np.ndarray,
 ) -> Dict[str, Any]:
-    m = MinHash(num_perm=num_perm, seed=MINHASH_SEED)
+    """
+    Combined with some datasketch code to avoid dependency.
+
+    Parameters
+    ----------
+    content : str
+        The content to be embedded.
+    idx : int
+        The index of the content.
+    num_perm : int
+        The number of permutations.
+    ngram_size : int
+        The size of n-grams.
+    hashranges : List[Tuple[int, int]]
+        The ranges of hash values.
+    permutations : np.ndarray
+        The permutations for the minhash.
+
+    Returns
+    -------
+    Dict[str, Any]
+        The hash values in each range and the index.
+    """
+    hashvalues = np.ones(num_perm, dtype=np.uint64) * MAX_HASH
     tokens = {" ".join(t) for t in ngrams(NON_ALPHA.split(content), ngram_size)}
-    m.update_batch([token.encode("utf-8") for token in tokens])
-    Hs = [bytes(m.hashvalues[start:end].byteswap().data) for start, end in hashranges]
+    hv = np.array([sha1_hash32(token.encode("utf-8")) for token in tokens], dtype=np.uint64)
+    a, b = permutations
+    phv = np.bitwise_and(((hv * np.tile(a, (len(hv), 1)).T).T + b) % MERSENNE_PRIME, MAX_HASH)
+    hashvalues = np.vstack([phv, hashvalues]).min(axis=0)
+    Hs = [bytes(hashvalues[start:end].byteswap().data) for start, end in hashranges]
     return {"__signatures__": Hs, "__id__": idx}
+
+
+def optimal_param(
+    threshold: float, num_perm: int, false_positive_weight: float = 0.5, false_negative_weight: float = 0.5
+):
+    """
+    Compute the optimal `MinHashLSH` parameter that minimizes the weighted sum
+    of probabilities of false positive and false negative, taken from datasketch.
+
+    Parameters
+    ----------
+    threshold : float
+        The threshold for similarity.
+    num_perm : int
+        The number of permutations.
+    false_positive_weight : float
+        The weight of false positive.
+    false_negative_weight : float
+        The weight of false negative.
+
+    Returns
+    -------
+    Tuple[int, int]
+        The optimal `b` and `r` parameters.
+    """
+
+    def false_positive_probability(threshold: float, b: int, r: int):
+        """Source: `datasketch.lsh`"""
+        proba = lambda s: 1 - (1 - s ** float(r)) ** float(b)
+        a, _ = integrate(proba, 0.0, threshold)
+        return a
+
+    def false_negative_probability(threshold: float, b: int, r: int):
+        """Source: `datasketch.lsh`"""
+        proba = lambda s: 1 - (1 - (1 - s ** float(r)) ** float(b))
+        a, _ = integrate(proba, threshold, 1.0)
+        return a
+
+    min_error = float("inf")
+    opt = (0, 0)
+    for b in range(1, num_perm + 1):
+        max_r = int(num_perm / b)
+        for r in range(1, max_r + 1):
+            fp = false_positive_probability(threshold, b, r)
+            fn = false_negative_probability(threshold, b, r)
+            error = fp * false_positive_weight + fn * false_negative_weight
+            if error < min_error:
+                min_error = error
+                opt = (b, r)
+    return opt
 
 
 class UnionFind:
@@ -84,12 +205,15 @@ if __name__ == "__main__":
         OUTPUT_BASE = Path(output or "output")
         OUTPUT_BASE.mkdir(exist_ok=True, parents=True)
         output = OUTPUT_BASE / "deduplicated"
+
+        logging.basicConfig(level=logging.INFO)
+
         time_measures = {}
         start_time = time.time()
-        lsh = MinHashLSH(
-            threshold=threshold,
-            num_perm=num_perm,
-        )
+
+        B, R = optimal_param(threshold, num_perm)
+        HASH_RANGES = [(i * R, (i + 1) * R) for i in range(B)]
+        HASH_TABLES = [defaultdict(set) for _ in range(B)]
 
         time_measures["load_dataset"] = time.time()
         ds = load_dataset(
@@ -104,6 +228,13 @@ if __name__ == "__main__":
         )
         time_measures["load_dataset"] = time.time() - time_measures["load_dataset"]
         DATA_SIZE = len(ds)
+        PERMUTATIONS = np.array(
+            [
+                (RNG.randint(1, MERSENNE_PRIME, dtype=np.uint64), RNG.randint(0, MERSENNE_PRIME, dtype=np.uint64))
+                for _ in range(num_perm)
+            ],
+            dtype=np.uint64,
+        ).T
 
         time_measures["minhash"] = time.time()
         embedded = ds.map(
@@ -111,8 +242,9 @@ if __name__ == "__main__":
             fn_kwargs={
                 "num_perm": num_perm,
                 "ngram_size": num_perm,
-                "hashranges": lsh.hashranges,
+                "hashranges": HASH_RANGES,
                 "ngram_size": ngram_size,
+                "permutations": PERMUTATIONS,
             },
             input_columns=[column],
             remove_columns=ds.column_names,
@@ -122,18 +254,17 @@ if __name__ == "__main__":
         )
         time_measures["minhash"] = time.time() - time_measures["minhash"]
 
-        # TODO: if one day, the map function supports async mode, we can use it to speed up the following step
+        # This is a greedy algorithm
         time_measures["clustering"] = time.time()
         uf = UnionFind()
         for record in tqdm(embedded, dynamic_ncols=True, desc="Clustering..."):
             key = record["__id__"]
             Hs = record["__signatures__"]
-            for H, hashtable in zip(Hs, lsh.hashtables):
-                for candidate in hashtable.get(H):
+            for H, hashtable in zip(Hs, HASH_TABLES):
+                for candidate in hashtable[H]:
                     uf.union(candidate, key)
-            lsh.keys.insert(key, *Hs, buffer=False)
-            for H, hashtable in zip(Hs, lsh.hashtables):
-                hashtable.insert(H, key, buffer=False)
+            for H, hashtable in zip(Hs, HASH_TABLES):
+                hashtable[H].add(key)
         time_measures["clustering"] = time.time() - time_measures["clustering"]
 
         time_measures["filtering"] = time.time()
