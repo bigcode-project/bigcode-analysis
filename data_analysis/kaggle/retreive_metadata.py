90c340fda5b8c7bac65d77fd6785caf4ca2b6924
loubnabnl <loubnabenallal1999@gmail.com>
2023-09-21 16:09:43 +0000

    update code

--- data_analysis/kaggle/retreive_metadata.py
+++ data_analysis/kaggle/retreive_metadata.py
@@ -3,9 +3,11 @@ import pandas as pd
 import numpy as np
 import json
 from pathlib import Path
+from datasets import load_dataset, Dataset
 
+ds = load_dataset("bigcode/kaggle-notebooks-data", use_auth_token=True, split="train")
+print("dataset loaded")
 
-code_base_path = Path('/fsx/loubna/kaggle_data/kaggle-code-data/data')
 kv_csv = '/fsx/loubna/kaggle_data/metadata_kaggle/KernelVersions.csv'
 kernelversions_datasetsources_csv = '/fsx/loubna/kaggle_data/metadata_kaggle/KernelVersionDatasetSources.csv'
 datasets_versions_csv = '/fsx/loubna/kaggle_data/metadata_kaggle/DatasetVersions.csv'
@@ -17,27 +19,51 @@ datasets_versions = pd.read_csv(datasets_versions_csv)
 datasets = pd.read_csv(datasets_csv)
 kernelversions_datasetsources = pd.read_csv(kernelversions_datasetsources_csv)
 users = pd.read_csv(users_csv)
+print("metadata loaded")
 
+def safe_get(dataframe, condition, column=None):
+    """Utility function to safely get value from DataFrame."""
+    result = dataframe[condition]
+    if result.empty:
+        return None
+    if column:
+        return result[column].values[0]
+    return result
 
 def get_metadata(file_id):
     """given the id of a notebook (=the stem of its path) we retrieve metadata from csv tables
     provided by kaggle"""
-    output = {}
-    kversion = kversions[kversions['Id']==int(file_id)]
-    data_source_kernel = kernelversions_datasetsources[kernelversions_datasetsources['KernelVersionId']==int(file_id)]
-    source_id = data_source_kernel['SourceDatasetVersionId']
-    dataset_v = datasets_versions[datasets_versions['Id']==int(source_id)]
-    data_name = dataset_v["Slug"].values[0]
-    source_dataset = datasets[datasets['Id']==int(dataset_v["DatasetId"])]
-    user = users[users['Id']==int(source_dataset["OwnerUserId"])]
-    final_data = f'{user["UserName"].values[0]}/{data_name}'
 
-    output['kaggle_dataset_name'] = data_name
-    output['kaggle_dataset_owner'] = user["UserName"].values[0]
-    output['kversion'] = kversion.to_dict(orient='records')
-    output['kversion_datasetsources'] = data_source_kernel.to_dict(orient='records')
-    output['dataset_versions'] = dataset_v.to_dict(orient='records')
-    output['datasets'] = source_dataset.to_dict(orient='records')
-    output['users'] = user.to_dict(orient='records')
+    file_id_int = int(file_id)
+    kversion = safe_get(kversions, kversions['Id'] == file_id_int)
+    data_source_kernel = safe_get(kernelversions_datasetsources, kernelversions_datasetsources['KernelVersionId'] == file_id_int)
+    
+    source_id = None if data_source_kernel is None else data_source_kernel['SourceDatasetVersionId'].values[0]
+    dataset_v = safe_get(datasets_versions, datasets_versions['Id'] == source_id)
+    
+    data_name = dataset_v["Slug"].values[0] if dataset_v is not None else None
+    dataset_id = dataset_v["DatasetId"].values[0] if dataset_v is not None else None
+    
+    source_dataset = safe_get(datasets, datasets['Id'] == dataset_id)
+    owner_user_id = None if source_dataset is None else source_dataset["OwnerUserId"].values[0]
+    
+    user = safe_get(users, users['Id'] == owner_user_id)
+    user_name = None if user is None else user["UserName"].values[0]
 
+    return {
+        'kaggle_dataset_name': data_name,
+        'kaggle_dataset_owner': user_name,
+        'kversion': json.dumps(kversion.to_dict(orient='records')) if kversion is not None else None,
+        'kversion_datasetsources': json.dumps(data_source_kernel.to_dict(orient='records')) if data_source_kernel is not None else None,
+        'dataset_versions': json.dumps(dataset_v.to_dict(orient='records')) if dataset_v is not None else None,
+        'datasets': json.dumps(source_dataset.to_dict(orient='records')) if source_dataset is not None else None,
+        'users': json.dumps(user.to_dict(orient='records')) if user is not None else None
+    }
+
+
+def retrive_metadata(row):
+    output = get_metadata(row['file_id'])
     return output
+
+# issue when using amp new values are None, but aren't without it?
+ds = ds.map(retrive_metadata, num_proc=36)
